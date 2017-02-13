3e9b5d441714213c8bb8d6223ebe8bc414ded374
dmsch <dschukin@mail.ru>
2017-02-13 22:34:47 +0200

    2-4

--- assets/js/app/controllers/PageController.js
+++ assets/js/app/controllers/PageController.js
@@ -3,12 +3,22 @@
 angular.module('minmax')
 .controller('MinMaxCtrl', MinMaxCtrl);
 
-MinMaxCtrl.$inject = ['$scope'];
-function MinMaxCtrl($scope) {
+MinMaxCtrl.$inject = ['$scope', '$http'];
+function MinMaxCtrl($scope, $http) {
   $scope.formModel = {};
 
   $scope.onSubmit = function () {
     console.log("Hey, I'm submitted!");
     console.dir($scope.formModel);
+
+    $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel)
+      .then(successCb, errorCb);
+    function successCb(data) {
+      console.log(':)');
+    }
+
+    function errorCb(data) {
+      console.log(':(');
+    }
   }
 }
