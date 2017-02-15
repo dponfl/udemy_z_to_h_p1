64f7a0eb9e16f771e3cd6a1a92485dc7eacffdfc
dmsch <dschukin@mail.ru>
2017-02-15 23:06:00 +0200

    2-6

--- assets/js/app/controllers/PageController.js
+++ assets/js/app/controllers/PageController.js
@@ -7,13 +7,20 @@ MinMaxCtrl.$inject = ['$scope', '$http'];
 function MinMaxCtrl($scope, $http) {
   $scope.formModel = {};
 
-  $scope.onSubmit = function () {
-    console.log("Hey, I'm submitted!");
-    console.dir($scope.formModel);
+  $scope.onSubmit = function (valid) {
+    if (valid) {
+      console.log("Hey, I'm submitted!");
+      console.dir($scope.formModel);
 
-    // $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel)
-    $http.post('http://localhost:1337/test', $scope.formModel)
-      .then(successCb, errorCb);
+      // $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel)
+      $http.post('http://localhost:1337/test', $scope.formModel)
+        .then(successCb, errorCb);
+    }
+    else {
+      console.log('Invalid form!');
+      $scope.emailValid = false;
+    }
+
     function successCb(data) {
       console.log(':)');
       console.dir(data);
