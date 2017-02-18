cb677f8dca9127dd1fff5de047c3bccaa5b38320
dmsch <dschukin@mail.ru>
2017-02-18 14:06:01 +0200

    project inital phase

--- assets/js/app/controllers/PageController.js
+++ assets/js/app/controllers/PageController.js
@@ -1,37 +1,8 @@
 "use strict";
 
-angular.module('minmax')
-.controller('MinMaxCtrl', MinMaxCtrl);
+angular.module('app')
+.controller('SomeCtrl', SomeCtrl);
 
-MinMaxCtrl.$inject = ['$scope', '$http'];
-function MinMaxCtrl($scope, $http) {
-  $scope.formModel = {};
-  $scope.submitting = false;
-
-  $scope.onSubmit = function () {
-    $scope.submitting = true;
-    console.log("Hey, I'm submitted!");
-    console.dir($scope.formModel);
-
-    $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel)
-    // $http.post('http://localhost:1337/test', $scope.formModel)
-      .then(successCb, errorCb);
-
-/*
-    setTimeout(function () {
-      $http.post('http://localhost:1337/test', $scope.formModel)
-        .then(successCb, errorCb);
-    }, 3000);
-*/
-
-    function successCb() {
-      $scope.submitting = false;
-      console.log(':)');
-    }
-
-    function errorCb() {
-      $scope.submitting = false;
-      console.log(':(');
-    }
-  }
+SomeCtrl.$inject = ['$scope'];
+function SomeCtrl($scope) {
 }
--- assets/js/app/module.js
+++ assets/js/app/module.js
@@ -2,47 +2,6 @@
 
 (function () {
 
-  /*angular.module('Project1', [
-    'ngRoute',
-    'ngResource'
-  ]).config(ProjectConfig);
-
-  ProjectConfig.$inject = ['$routeProvider','$locationProvider'];
-  function ProjectConfig($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/', {
-      templateUrl: 'templates/view/home.html',
-    })
-      .when('/test', {
-        templateUrl: 'templates/view/test.html'
-      })
-      .when('/404', {
-        templateUrl: 'templates/view/404.html'
-      })
-      .otherwise({
-      redirectTo: '/404'
-    });
-
-    $locationProvider.hashPrefix('');
-  }*/
-
-  angular.module('minmax', [
-    'jcs-autoValidate',
-    'angular-ladda',
-  ])
-    .run(autoValidateDefaultErrorMessageResolver);
-
-  autoValidateDefaultErrorMessageResolver.$inject = ['defaultErrorMessageResolver'];
-  function autoValidateDefaultErrorMessageResolver(defaultErrorMessageResolver) {
-    defaultErrorMessageResolver
-      .getErrorMessages()
-      .then(function (errorMessages) {
-        errorMessages['tooYoung'] = 'You must be at least {0} years old to use this site';
-        errorMessages['tooOld'] = 'You must be max {0} years old to use this site';
-        errorMessages['badUsername'] = 'Username can only contain numbers, letters and _';
-        errorMessages['tooShortUsername'] = 'Username must be at least {0} characters long';
-        errorMessages['pwTooShort'] = 'Password must be at least {0} characters long';
-      })
-  }
+  angular.module('app', [])
 
 })();
