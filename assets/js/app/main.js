193238aa880350a408413835ca3176e5ab69fd3d
dmsch <dschukin@mail.ru>
2017-02-19 19:06:02 +0200

    7-4

--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -2,6 +2,14 @@
 
 (function () {
 
-  angular.module('codecraft', []);
+  angular.module('codecraft', [
+    'ngResource'
+  ]).config(CodecraftConfig);
+
+  CodecraftConfig.$inject = ['$httpProvider', '$resourceProvider'];
+  function CodecraftConfig($httpProvider, $resourceProvider) {
+    $httpProvider.defaults.headers.common['Authorization'] = 'Token 186cca3d861a9409aba5f7720623cad0b633828e';
+    $resourceProvider.defaults.stripTrailingSlashes = false;
+  }
 
 })();
